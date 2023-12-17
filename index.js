f25df94cce58a2e9cf0008db4c039f602e008fb0
是你李某人 <1677712032@qq.com>
2023-12-17 18:15:38 +0800

    change layout

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ const img_profile_src = 'https://avatars.githubusercontent.com/u/91362332';
 
 const img_profile = document.getElementById('img_profile');
 
-
 img_profile.setAttribute('src', img_profile_src);
+
+
+window.addEventListener('load', e => {
+    // const hostname = window.location.hostname;
+    const hostname = "embeddedalchemist.github.io";
+    // fetch(`https://finicounter.eu.org/counter?host=${hostname}`)
+    //     .then((response) => {
+    //         return response.json();
+    //     })
+    //     .then((response) => { 
+    //         console.log(response);
+    //     })
+})
--- loading_screen.js
+++ loading_screen.js
@@ -3,23 +3,19 @@ const loading_mask = document.getElementById('loading_mask');
 const loading_tips = document.getElementById('loading_tips');
 const tips_text = [
     { text: "Nice to see you!", timeout: 2500, next: 1 },
-    { text: "Resources are still loading...", timeout: 3000, next: 2 },
-    { text: "Just a few seconds...", timeout: 4000, next: 3 },
+    { text: "Just a few seconds...", timeout: 4000, next: 2 },
+    { text: "Resources are still loading...", timeout: 5000, next: 3 },
     { text: "It might take a while...", timeout: 5000, next: 4 },
-    { text: "You may ckeck your network.", timeout: 5000, next: 5 },
+    { text: "Still loading... You may ckeck your network.", timeout: 5000, next: 5 },
     { text: "在中国大陆，你可能需要使用VPN才能访问这个页面", timeout: 5000, next: 4 },
 ];
-let inner_timer_id = 0;
 const text_set_func = (text) => {
-    loading_tips.style.filter = "blur(3px)";
-    loading_tips.style.transform = "scale(0.95)";
-    loading_tips.style.opacity = 0;
-    inner_timer_id = setTimeout(() => {
+    loading_tips.style.animation = "0.5s fade-out"
+    animation_end_cb = loading_tips.addEventListener('animationend', e => {
+        loading_tips.removeEventListener('animationend', loading_tips);
         loading_tips.innerHTML = text;
-        loading_tips.style.filter = "blur(0px)";
-        loading_tips.style.transform = "scale(1)";
-        loading_tips.style.opacity = 1;
-    }, 500);
+        loading_tips.style.animation = "0.5s fade-in"
+    })
 }
 const text_switch_func = (text_arr, index) => {
     text_set_func(text_arr[index].text)
@@ -31,7 +27,6 @@ let timer_id = setTimeout(text_switch_func, 1000, tips_text, 0);
 
 window.addEventListener('load', (e) => {
     clearTimeout(timer_id);
-    clearTimeout(inner_timer_id);
     text_set_func("Let's GO!")
     setTimeout(() => {
         loading_mask.removeAttribute("display-flag");
