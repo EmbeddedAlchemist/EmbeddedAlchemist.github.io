a77c83b282b23ce15aeaa01d3720b7e8db08f368
是你李某人 <1677712032@qq.com>
2023-12-23 20:56:40 +0800

    change background

--- loading_screen.js
+++ loading_screen.js
@@ -7,7 +7,10 @@ const tips_text = [
     { text: "Resources are still loading...", timeout: 5000, next: 3 },
     { text: "It might take a while...", timeout: 5000, next: 4 },
     { text: "Still loading... You may ckeck your network.", timeout: 5000, next: 5 },
-    { text: "在中国大陆，你可能需要使用VPN才能访问这个页面", timeout: 5000, next: 4 },
+    { text: "在中国大陆，你可能需要绕过GFW才能访问这个页面", timeout: 5000, next: 6 },
+    { text: "Github上不去是常有的事", timeout: 3000, next: 7 },
+    { text: "问问你计算机专业的朋友/同事，TA一定知道怎么处理", timeout: 5000, next: 4 },
+
 ];
 const text_set_func = (text) => {
     loading_tips.style.animation = "0.5s fade-out"
@@ -26,9 +29,15 @@ const text_switch_func = (text_arr, index) => {
 let timer_id = setTimeout(text_switch_func, 1000, tips_text, 0);
 
 window.addEventListener('load', (e) => {
-    clearTimeout(timer_id);
-    text_set_func("Let's GO!")
-    setTimeout(() => {
-        loading_mask.removeAttribute("display-flag");
-    }, 850);
+    // setTimeout(() => {
+        clearTimeout(timer_id);
+        text_set_func("Let's GO!")
+        setTimeout(() => {
+            loading_mask.removeAttribute("display-flag");
+            setTimeout(() => {
+                loading_mask.style.display = 'none';
+            }, 1500);
+        }, 850);
+    // }, 10000000);
 })
+
